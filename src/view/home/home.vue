0e20c1cc2203e9c6812e70b38f45b4cc97efe4d6
Joeng3ll <272679540@qq.com>
2017-05-15 20:46:59 +0800

    下拉刷新

--- src/view/home/home.vue
+++ src/view/home/home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home-wrapper">
     <header class="header">
-      <section class="msg">
+      <section class="msg icon-sec">
         <msg-cmp :msgCount="10"></msg-cmp>
       </section>
       <section class="title">
         首页
       </section>
-      <section class="scan">
+      <section class="scan icon-sec">
         <i class="icon-xiaoxi"></i>
       </section>
     </header>
@@ -19,51 +19,51 @@
     <article class="body" ref="newsBody">
       <div class="slide-body">
         <!--新闻banner图开始-->
-        <aside class="news-banner swiper-container" ref="swiperContainer">
-          <!--swiper滑动部分开始-->
-          <div class="swiper-wrapper">
-            <img :src="currentNews.thumbnail_pic_s" class="swiper-slide news-pic">
-            <img :src="currentNews.thumbnail_pic_s02" class="swiper-slide news-pic">
-            <img :src="currentNews.thumbnail_pic_s03" class="swiper-slide news-pic">
-          </div>
-          <!--swiper滑动部分结束-->
-          <section class="swiper-pagination"></section><!--swiper分页-->
-          <!--新闻标题-->
-          <section class="news-title">
-            <p class="title-text">{{currentNews.title}}</p>
-          </section>
-        </aside>
-        <!--新闻banner图结束-->
-        <!--二级菜单开始-->
-        <nav class="nav-wrapper">
-          <!--表彰奖励-->
-          <section class="nav-item-box">
-            <div class="nav-item">
-              <div class="icon-box">
-                <i class="icon-iconfontjiangli icon"></i>
-              </div>
-              <div class="nav-text">
-                <p>表彰奖励</p>
-                <span>honor</span>
-              </div>
-            </div>
-          </section>
-          <!--事故通报-->
-          <section class="nav-item-box">
-            <div class="nav-item">
-              <div class="icon-box accident">
-                <i class="icon-jiaotongshigu icon"></i>
-              </div>
-              <div class="nav-text">
-                <p>事故通报</p>
-                <span>accident</span>
-              </div>
-            </div>
-          </section>
-        </nav>
-        <!--二级菜单结束-->
-        <!--新闻列表开始-->
         <mt-loadmore :top-method="loadTop" ref="loadmore">
+          <aside class="news-banner swiper-container" ref="swiperContainer">
+            <!--swiper滑动部分开始-->
+            <div class="swiper-wrapper">
+              <img :src="currentNews.thumbnail_pic_s" class="swiper-slide news-pic">
+              <img :src="currentNews.thumbnail_pic_s02" class="swiper-slide news-pic">
+              <img :src="currentNews.thumbnail_pic_s03" class="swiper-slide news-pic">
+            </div>
+            <!--swiper滑动部分结束-->
+            <section class="swiper-pagination"></section><!--swiper分页-->
+            <!--新闻标题-->
+            <section class="news-title">
+              <p class="title-text">{{currentNews.title}}</p>
+            </section>
+          </aside>
+          <!--新闻banner图结束-->
+          <!--二级菜单开始-->
+          <nav class="nav-wrapper">
+            <!--表彰奖励-->
+            <section class="nav-item-box">
+              <div class="nav-item">
+                <div class="icon-box">
+                  <i class="icon-iconfontjiangli icon"></i>
+                </div>
+                <div class="nav-text">
+                  <p>表彰奖励</p>
+                  <span>honor</span>
+                </div>
+              </div>
+            </section>
+            <!--事故通报-->
+            <section class="nav-item-box">
+              <div class="nav-item">
+                <div class="icon-box accident">
+                  <i class="icon-jiaotongshigu icon"></i>
+                </div>
+                <div class="nav-text">
+                  <p>事故通报</p>
+                  <span>accident</span>
+                </div>
+              </div>
+            </section>
+          </nav>
+          <!--二级菜单结束-->
+          <!--新闻列表开始-->
           <div class="news-list">
             <section class="news-item" v-for="news in newsList">
               <!--新闻主图-->
@@ -116,9 +116,9 @@
 //       不显示已经在banner里出现的新闻
         this.newsList.shift()
         this.currentNews = res[0]
-        this.$nextTick(() => {
-          this._initialBScroll()
-        })
+//        this.$nextTick(() => {
+//          this._initialBScroll()
+//        })
       })
       this.$nextTick(() => {
         this._initialSwiper()
@@ -177,10 +177,12 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .home-wrapper
     & > .header
+      position: fixed
+      z-index: 100
+      width: 100%
       display flex
       justify-content space-between
       height 1.1733rem
-      padding 0 .48rem
       background rgb(58, 153, 240)
       font-size 0
       color #fff
@@ -188,103 +190,108 @@
       & > section
         display inline-block
         line-height 1.1733rem
-
+        &.icon-sec
+          padding 0 .48rem
       & > .title
         font-size 18px
 
     & > .news-nav
+      position fixed
+      width 100%
+      top 1.1733rem
+      left 0
+      z-index 100
       background rgb(248, 248, 248)
       box-shadow 0px 0px 5px rgba(0, 0, 0, 0.1)
     & > .body
-      position: fixed
+      z-index -1
       width 100%
-      left 0
-      bottom: 0
-      top: 2.3466rem
       padding-bottom: 1.30667rem
-      z-index: -1
+      padding-top 2.3466rem
       background: #fff
       & > .slide-body
-        padding-bottom: 1.30667rem
-        & > .news-banner
-          width 100%
-          height 5.5rem
-          position relative
-          font-size 0
-          overflow hidden
-          & > .swiper-wrapper
-            width auto
-            height 100%
-            & > .news-pic
-              display inline-block
-              height 100%
-          & > .swiper-pagination
-            position absolute
-            bottom .4rem
-            right .3rem
-            z-index 100
-            font-size 0
-            & > .swiper-pagination-bullet
-              display inline-block
-              padding .08rem
-              background #bbbbbb
-              border-radius 100%
-              &:not(:last-child)
-                margin-right .1rem
-              &.swiper-pagination-bullet-active
-                border-radius 50%
-                background #fff
-          & > .news-title
-            width 100%
-            background rgba(0, 0, 0, 0.4)
-            & > .title-text
-              font-size 16px
-              white-space nowrap
-              overflow hidden
-              text-overflow ellipsis
-              letter-spacing .02rem
-              padding .3rem 1.7rem .3rem .3rem
-              color #f0f0f0
-          & > .news-title
-            position absolute
-            bottom 0
-
-        & > .nav-wrapper
-          display flex
-          & > .nav-item-box
-            flex 1
-            text-align center
-            border-bottom .01rem solid #dddddd
-            & > .nav-item
-              display flex
-              justify-content center
-              padding .2933rem
-              &:first-child
-                border-left .01rem solid #dddddd
-              & > .icon-box
-                padding .2933rem
-                margin-right .2rem
-                background rgb(208, 55, 64)
-                border-radius 50%
-                &.accident
-                  background rgb(110, 128, 162)
-                & > .icon
-                  font-size 24px
-                  color #f0f0f0
-              & > .nav-text
-                display flex
-                flex-direction column
-                justify-content center
-                -webkit-justify-content center
-                align-items center
-                -webkit-align-items center
-                & > p
-                  font-size 16px
-                & > span
-                  margin-top .1rem
-                  color #ccc
         & > .mint-loadmore
           & > .mint-loadmore-content
+            & > .mint-loadmore-top
+              & > .mint-loadmore-text
+                color #ccc
+            & > .news-banner
+              width 100%
+              height 5.5rem
+              position relative
+              font-size 0
+              overflow hidden
+              & > .swiper-wrapper
+                width auto
+                height 100%
+                & > .news-pic
+                  display inline-block
+                  height 100%
+              & > .swiper-pagination
+                position absolute
+                bottom .4rem
+                right .3rem
+                z-index 100
+                font-size 0
+                & > .swiper-pagination-bullet
+                  display inline-block
+                  padding .08rem
+                  background #bbbbbb
+                  border-radius 100%
+                  &:not(:last-child)
+                    margin-right .1rem
+                  &.swiper-pagination-bullet-active
+                    border-radius 50%
+                    background #fff
+              & > .news-title
+                width 100%
+                background rgba(0, 0, 0, 0.4)
+                & > .title-text
+                  font-size 16px
+                  white-space nowrap
+                  overflow hidden
+                  text-overflow ellipsis
+                  letter-spacing .02rem
+                  padding .3rem 1.7rem .3rem .3rem
+                  color #f0f0f0
+              & > .news-title
+                position absolute
+                bottom 0
+
+            & > .nav-wrapper
+              display flex
+              & > .nav-item-box
+                flex 1
+                text-align center
+                border-bottom .01rem solid #dddddd
+                & > .nav-item
+                  display flex
+                  justify-content center
+                  padding .2933rem
+                  &:first-child
+                    border-left .01rem solid #dddddd
+                  & > .icon-box
+                    padding .2933rem
+                    margin-right .2rem
+                    background rgb(208, 55, 64)
+                    border-radius 50%
+                    &.accident
+                      background rgb(110, 128, 162)
+                    & > .icon
+                      font-size 24px
+                      color #f0f0f0
+                  & > .nav-text
+                    display flex
+                    flex-direction column
+                    justify-content center
+                    -webkit-justify-content center
+                    align-items center
+                    -webkit-align-items center
+                    & > p
+                      font-size 16px
+                    & > span
+                      margin-top .1rem
+                      color #ccc
             & > .news-list
               & > .news-item
                 position relative
