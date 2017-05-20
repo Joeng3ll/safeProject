1b22fb7709d2c7dad25e43c2df1900cd8ab43cff
Joeng3ll <272679540@qq.com>
2017-05-20 15:04:14 +0800

    dist

---
+++ src/service/dataTemplate/hourRecord.js
@@ -0,0 +1,14 @@
+/**
+ * Created by joeng on 2017/5/19.
+ */
+let template = {
+  'transport_record':{
+    "driver_id":1,
+    "work_hour":8,
+    "year_hour":360,
+    "month_hour":24,
+    "attendance_days":[
+
+    ]
+  }
+}
